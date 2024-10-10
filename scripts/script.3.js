5d48cd6ebf89fff3dfcd9c55788bc4edb8d591b4
Duda <dudagcamargos037@gmail.com>
2024-10-10 14:41:16 -0300

    Update script.3.js

--- scripts/script.3.js
+++ scripts/script.3.js
@@ -1,14 +1,25 @@
 let carros = [];
 let adicionarMais = true;
 while (adicionarMais) {
-  carros.push({
-    modelo: prompt("Modelo do carro:"),
-    marca: prompt("Marca do carro:"),
-    ano: parseInt(prompt("Ano do carro:")),
-    valor: parseFloat(prompt("Valor do carro:"))
-  });
-  adicionarMais = confirm("Adicionar outro carro?");
+  let carros={
+    modelo: prompt(" Digite o modelo do carro:"),
+    marca: prompt("Digite a marca do carro:"),
+    ano: parseInt(prompt("Digite o ano do carro:")),
+    valor: parseFloat(prompt("Digite o valor do carro:"))
+  };
+  carros.push(carro);
+    let resposta =prompt("Deseja adicionar mais carros?(sim/não)");
+   if(resposta!= "sim"){
+   adicionarMais = false;
+}
+}
+     let carroMaisCaro = carros[0];
+     for (let i=1; i< carros.length; i++){
+     if(carros[i].valor> carroMaisCaro.valor){
+
+     carroMaisCaro = carros[i];
+  }
+
 }
 
-let carroMaisCaro = carros.reduce((maisCaro, carro) => carro.valor > maisCaro.valor ? carro : maisCaro);
-console.log(carroMaisCaro);
+console.log("O carro mais caro é:", carroMaisCaro);
